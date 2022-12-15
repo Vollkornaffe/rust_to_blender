e50d3f7d494609f8da2a6b5babfd3fe643033619
Vollkornaffe <Vollkornaffe@gmail.com>
2022-12-15 19:14:53 +0100

    Add simple rust program that writes to fifo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Write},
+};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    blender_input_fifo: String,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
+    println!("opening");
+    let mut fifo = OpenOptions::new()
+        .write(true)
+        .open(args.blender_input_fifo)?;
+
+    println!("writing");
+    fifo.write(b"Hello from rust")?;
+    fifo.flush()?;
+
+    Ok(())
 }
